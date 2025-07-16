e1aed37aa8cc6ee01173bca5b7c6b36e8198e693
lattitudehawk <mewhib7@gmail.com>
2025-07-16 18:39:00 +0500

    jkjsdkl

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Component/Header'
 const Home = () => {
+  const [isHovered, setIshovered] = useState(false);
+
   return (
     <div className="min-h-screen w-full bg-[url('C:\Users\Dell\Desktop\project\project_a\src\assets\graphics\landscape.jpg')]">
-        <div className='flex-col flex min-h-screen justify-center items-center'>
+        <div className='flex-col flex min-h-screen justify-center items-center  '>
           <h2 className='text-3xl text-black text-center w-200 bg-white/1'>
             "Travel isn’t always pretty. It isn’t always comfortable. But that’s okay. The journey changes you."
             </h2>
-            <button className='font-bold text-3xl border-white border-2 rounded-2xl px-4 py-1 hover:text-white cursor-pointer hover:px-6 duration-250 bg-white/20 mt-5'>locations</button>
+            <div
+            onMouseEnter={() => setIshovered(true)}
+            onMouseLeave={() => setIshovered(false)}
+            >
+              
+              
+            <button className=' font-bold text-3xl border-white border-2 rounded-2xl px-4 py-1 hover:text-white cursor-pointer hover:px-6 duration-250 bg-white/20 mt-3'>locations</button>
+            <div className={` absolute bg-white/40 rounded-1.5x transition-all duration-700 overflow-hidden ${isHovered ? 'w-64 opacity-100 visible':'w-0 opacity-0 max-h-0 invisible' }`}>
+              <ul className=''>
+                <li className='mt-0.5 mb-0.5 mr-2 ml-2 border-1 px-6 py-1 hover:bg-white/50 font-bold'>japan</li>
+                <li className='mt-0.5 mb-0.5 mr-2 ml-2 border-1 px-6 py-1 hover:bg-white/50 font-bold'>indonesia</li>
+                <li className='mt-0.5 mb-0.5 mr-2 ml-2 border-1 px-6 py-1 hover:bg-white/50 font-bold'>korea</li>
+                <li className='mt-0.5 mb-0.5 mr-2 ml-2 border-1 px-6 py-1 hover:bg-white/50 font-bold'>china</li>                
+              </ul>
+            </div>
+            </div>
             <div>
+
               
             </div>
         </div>
